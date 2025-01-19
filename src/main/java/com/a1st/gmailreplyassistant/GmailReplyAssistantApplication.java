2670658f29609825cb025005823e6f3bb3606d35
Abderrahman Youabd <youabd50@gmail.com>
2025-01-19 01:47:38 +0100

    initial commit

---
+++ src/main/java/com/a1st/gmailreplyassistant/GmailReplyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.a1st.gmailreplyassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailReplyAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmailReplyAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a1st/gmailreplyassistant/GmailReplyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1st.gmailreplyassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailReplyAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
